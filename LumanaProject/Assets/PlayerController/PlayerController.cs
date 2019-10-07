94592a366a5b144ff83a6dcc20ad77e5ef45245d
github username <github email address>
2019-10-07 15:09:07 -0500

    Basic script done, adding in animations for visual feedback

--- LumanaProject/Assets/PlayerController/PlayerController.cs
+++ LumanaProject/Assets/PlayerController/PlayerController.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    private Animator characterAnimator;
+    public bool isGrounded = false;
+
+    void Start(){
+        characterAnimator = GetComponent<Animator>();
+    }
 
     public void PlayerMovement(){
         float moveHor = Input.GetAxis("Horizontal");
         float moveVert = Input.GetAxis("Vertical");
-        Vector3 playerMovement = new Vector3(moveHor , 0 , moveVert) * speed * Time.deltaTime;
+        Vector3 playerMovement = new Vector3(moveHor , 0 , moveVert).normalized * speed * Time.deltaTime;
         transform.Translate(playerMovement , Space.Self);
     }
 
